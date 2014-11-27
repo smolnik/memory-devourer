1910d693865c965f96df59a362492a074455614f
smolnik <smolnik@wp.pl>
2014-11-27 11:20:45 +0100

    eclipse project - initial commit

---
+++ src/net/adamsmolnik/DevourerServlet.java
@@ -0,0 +1,17 @@
+package net.adamsmolnik;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet({"/eat"})
+public class DevourerServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1453454354354L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        
+    }
+}
